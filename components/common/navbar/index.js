181e9f6f58c63b1eff9c271231bd6dbacc36834d
Utkarsh Rai <raiutkarsh554@gmail.com>
2023-10-31 09:07:49 +0530

    some minor changes

--- components/common/navbar/index.js
+++ components/common/navbar/index.js
@@ -1,36 +1,48 @@
-
-
-
-
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Footer() {
-
   return (
     <section>
       <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
         <nav className="relative" aria-label="Global">
           <div className="flex justify-between">
             <div>
-              <Link href="/" className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+              <Link
+                href="/"
+                className="font-medium mr-8 text-gray-500 hover:text-gray-900"
+              >
                 Home
               </Link>
-              <Link href="/" className="font-medium mr-8 text-gray-500 hover:text-gray-900">
-                  Marketplace
+              <Link
+                href="/"
+                className="font-medium mr-8 text-gray-500 hover:text-gray-900"
+              >
+                Marketplace
               </Link>
-              <Link href="/" className="font-medium mr-8 text-gray-500 hover:text-gray-900">
-                  Blogs
+              <Link
+                href="/"
+                className="font-medium mr-8 text-gray-500 hover:text-gray-900"
+              >
+                Blogs
               </Link>
             </div>
             <div>
-              <Link href="/" className="font-medium mr-8 text-gray-500 hover:text-gray-900">
-                  Wishlist
+              <Link
+                href="/"
+                className="font-medium mr-8 text-gray-500 hover:text-gray-900"
+              >
+                Wishlist
               </Link>
-              <a href="#" className="font-medium mr-8 text-indigo-600 hover:text-indigo-500">Log in</a>
+              <a
+                href="#"
+                className="px-8 py-3 border rounded-lg text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+              >
+                Connect
+              </a>
             </div>
           </div>
         </nav>
       </div>
     </section>
-  )
+  );
 }
