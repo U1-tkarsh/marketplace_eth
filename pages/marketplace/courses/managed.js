a1a2f2f98823c62910ceccd82e02c3696ce78269
Utkarsh Rai <raiutkarsh554@gmail.com>
2023-11-22 23:44:40 +0530

    withdraw functions and tests fixed!

--- pages/marketplace/courses/managed.js
+++ pages/marketplace/courses/managed.js
@@ -55,6 +55,7 @@ export default function ManagedCourses() {
   const [filters, setFilters] = useState({ state: "all" });
 
   const verifyCourse = (email, { hash, proof }) => {
+    if ( !email) return ;
     const emailHash = web3.utils.sha3(email);
     const proofToCheck = web3.utils.soliditySha3(
       { type: "bytes32", value: emailHash },
